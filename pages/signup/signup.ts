e001584b53edb058bf604071655847ef8fff946b
Shiroyasha15 <42787914+Shiroyasha15@users.noreply.github.com>
2018-11-27 10:08:14 -0200

    Add files via upload

--- pages/signup/signup.ts
+++ pages/signup/signup.ts
@@ -20,6 +20,9 @@ export class SignupPage {
     private authService: AuthService) {
   }
 
+
+  /** Aqui é onde criamos o usuário, não entendi a parte de estipular duração e o local de "alocação", mas após a confirmar o cadastro, um email de confirmação é enviado
+   * , conforme a configuração do firebase, e o usuário retorna para a homepage */
   createAccount() {
     if (this.form.form.valid) {
       let toast = this.toastCtrl.create({ duration: 3000, position: 'bottom' });
@@ -33,6 +36,8 @@ export class SignupPage {
 
           this.navCtrl.setRoot(HomePage);
         })
+
+        /**Aqui são tratados os erros, com as informações obtidas na documentação do fire base, é apresentada uma mensagem de acordo com cada problema encontrado(email em uso, caractere invalido, senha nao atende requisitos) */
         .catch((error: any) => {
           if (error.code  == 'auth/email-already-in-use') {
             toast.setMessage('O e-mail digitado já está em uso.');
